8502bc5c823269292dc947dbc8e17e0a89b7b5e8
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-23 16:32:23 +0530

    Update api.jsx

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,4 +1,4 @@
-const url = "http://localhost:5000"
+const url = import.meta.env.VITE_API_URL
 
 export function saveToken(token){
   localStorage.setItem("token", token)
@@ -8,17 +8,17 @@ export function getToken(){
   return localStorage.getItem("token")
 }
 
-export async function api(path, method, body){
-  const res = await fetch(url + path,{
+export async function api(path, method = "GET", body){
+  const res = await fetch(`${url}${path}`,{
     method,
     headers:{
       "Content-Type": "application/json",
       Authorization: getToken()
     },
-    body: body ? JSON.stringify(body) : null
+    body: body ? JSON.stringify(body) : undefined
   })
 
-  if (res.headers.get("content-type")?.includes("application/json")){
+  if(res.headers.get("content-type")?.includes("application/json")){
     return res.json()
   }
 }
